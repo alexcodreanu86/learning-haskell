f5eb8bcff8d3619009d3375ee03366d81de6d293
Alex Codreanu <alexcodreanu86@gmail.com>
2014-12-03 21:33:26 -0600

    prime factors kata

--- primeFactors/PrimeFactors.hs
+++ primeFactors/PrimeFactors.hs
@@ -1,3 +1,20 @@
 module PrimeFactors where
 primeFactors :: Int -> [Int]
-primeFactors x = [x]
+primeFactors x = getFactorsListForNumber x 2 []
+
+getFactorsListForNumber :: Int -> Int -> [Int] -> [Int]
+getFactorsListForNumber number divider currentFactors
+  | number < divider = currentFactors
+  | otherwise = if isNumberDivizibleBy divider number
+                then getFactorsListForNumber
+                        (div number divider)
+                        divider
+                        (currentFactors ++ [divider])
+                else
+                  getFactorsListForNumber
+                    number
+                    (succ divider)
+                    currentFactors
+
+isNumberDivizibleBy :: Int -> Int -> Bool
+isNumberDivizibleBy divider toDivide = toDivide `mod` divider == 0
--- primeFactors/PrimeFactorsTest.hs
+++ primeFactors/PrimeFactorsTest.hs
@@ -4,17 +4,33 @@ import Test.Hspec
 import PrimeFactors
 
 main :: IO ()
+
 main = hspec $ do
   describe "primeFactors" $ do
-    it "returns an [2] for 2" $ do
+    it "returns [] for any number smaller than 2" $
+      primeFactors 1 `shouldBe` []
+
+    it "returns [2] for 2" $
       primeFactors 2 `shouldBe` [2]
 
-    it "returns [3] for 3" $ do
+    it "returns [3] for 3" $
       primeFactors 3 `shouldBe` [3]
 
-    it "returns [2,2] for 4" $ do
+    it "returns [2,2] for 4" $
       primeFactors 4 `shouldBe` [2,2]
 
+    it "returns [3,3] for 4" $
+      primeFactors 9 `shouldBe` [3,3]
+
+    it "returns [2,2,3] for 12" $
+      primeFactors 12 `shouldBe` [2,2,3]
+
+    it "returns [2,3,3,3,5] for 270" $
+      primeFactors 270 `shouldBe` [2,3,3,3,5]
+
   describe "isNumberDivizibleBy" $ do
-    it "returns false when numbers are NOT divizible" $ do
-      isNumberDivizibleBy 10 
+    it "returns false when numbers are NOT divizible" $
+      isNumberDivizibleBy 3 10 `shouldBe` False
+
+    it "returns true when numbers are divizible" $
+      isNumberDivizibleBy 5 10 `shouldBe` True
